a155d2518b5ed4d231385355f2e4c2f68c2a88e5
martijnmichel <38880192+martijnmichel@users.noreply.github.com>
2023-08-12 08:02:14 +0200

    Update AppNavigation.tsx

--- src/components/application/AppNavigation.tsx
+++ src/components/application/AppNavigation.tsx
@@ -1,13 +1,20 @@
 import { createPortal } from "react-dom";
 import { AppMenu, AppMenuItem } from "./AppMenu";
 import { NavMenu, NavMenuItem } from "../../applications";
-import React from "react";
+import React, { useEffect } from "react";
 export const AppNavigation = ({ items }: { items: NavMenu[] }) => {
   const el = document.querySelector(`#active-app-navigation--portal`);
 
+  useEffect(() => {
+    return () => {
+      const $el = document.querySelector(`#app-nav--portal`);
+      if ($el) $el.remove();
+    };
+  }, []);
+
   if (el)
     return createPortal(
-      <div className="flex items-center gap-2">
+      <div id="app-nav--portal" className="flex items-center gap-2">
         {items.map((item) => (
           <AppMenu key={`menu-item-${item.label}`} title={item.label}>
             {item.items.map((menuItem: NavMenuItem, i: number) => {
